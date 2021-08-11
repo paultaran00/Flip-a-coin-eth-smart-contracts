52d0508eab408aeb2a80fbbb1f20b1e8f944ec7c
Paul <paultaran00@gmail.com>
2021-08-11 20:34:50 +0300

    styled input and cloud server connection

---
+++ cloud/cloud.js
@@ -0,0 +1,7 @@
+Moralis.Cloud.define("helloworld", async (request) => {
+    return "hellooogoo";
+});
+
+
+// use to compile and send to server
+// moralis-admin-cli watch-cloud-folder --moralisApiKey lUPMZBeuo1q0hxj --moralisApiSecret sIWenaTAO6uZLXL --moralisSubdomain qbgrfr4bspbb.moralisweb3.com --autoSave 1 --moralisCloudfolder "C:/Users/pol/Desktop/flipacoin/cloud"
